219868e812f1cd2fc5872486e66d5bc99e228bd2
Dieguidev <diegogaraycullas@gmail.com>
2024-07-23 19:34:20 -0500

    feat: Agregado el useWheather para realizar peticiones a la API

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,7 +4,12 @@ import styles from "./Form.module.css";
 import { SearchType } from "../../types";
 import { Alert } from "../Alert/Alert";
 
-export const Form = () => {
+type FormProps = {
+  fetchWeather: () => void;
+}
+
+
+export const Form = ({fetchWeather}: FormProps) => {
   const [search, setSearch] = useState<SearchType>({
     city: "",
     country: "",
@@ -27,6 +32,7 @@ export const Form = () => {
       setAlert("Todos los campos son obligatorios");
       return
     }
+    fetchWeather();
   };
 
   return (
