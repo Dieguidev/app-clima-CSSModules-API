8307a6985226439cec95f3e154b12b6bf1ff273c
Dieguidev <diegogaraycullas@gmail.com>
2024-07-23 20:09:33 -0500

    feat: Generada la peticion para la latitud y longitud

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,7 +5,7 @@ import { SearchType } from "../../types";
 import { Alert } from "../Alert/Alert";
 
 type FormProps = {
-  fetchWeather: () => void;
+  fetchWeather: (search: SearchType) => Promise<void>;
 }
 
 
@@ -32,7 +32,7 @@ export const Form = ({fetchWeather}: FormProps) => {
       setAlert("Todos los campos son obligatorios");
       return
     }
-    fetchWeather();
+    fetchWeather(search);
   };
 
   return (
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,8 +1,21 @@
+import axios from "axios";
+import { SearchType } from "../types";
 
 export default function useWeather() {
 
-  const fetchWeather = () => {
-    console.log("consultando...");
+  const appId = 'f00f99d71ae73aa3278b92588dee39f0'
+
+  const fetchWeather = async (search: SearchType) => {
+    try {
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+      const { data } = await axios(geoUrl)
+      console.log(data);
+
+
+    } catch (error) {
+      console.log(error);
+
+    }
 
   }
 
