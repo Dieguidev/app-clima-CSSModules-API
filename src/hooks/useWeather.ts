f806561b0a4e62e08c102b04ed507d9fe53effad
Dieguidev <diegogaraycullas@gmail.com>
2024-07-23 20:51:08 -0500

    feat: Refactor useWeather hook to improve type checking and error handling

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
 import { SearchType, Weather } from "../types";
 
+//*typeGuard o assertion
+function isWheaterResponse(weather: unknown): weather is Weather {
+  return (
+    Boolean(weather) &&
+    typeof weather === "object" &&
+    typeof (weather as Weather).name === "string" &&
+    typeof (weather as Weather).main.temp === "number" &&
+    typeof (weather as Weather).main.temp_min === "number" &&
+    typeof (weather as Weather).main.temp_max === "number"
+  )
+}
+
 export default function useWeather() {
 
   const appId = import.meta.env.VITE_API_KEY;
@@ -14,8 +26,18 @@ export default function useWeather() {
       const lon = data[0].lon;
 
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+      //* castear typt
+      // const { data: weatherData } = await axios<Weather>(weatherUrl)
+      // console.log(weatherData);
+
+      //* type Guards
       const { data: weatherData } = await axios<Weather>(weatherUrl)
-      console.log(weatherData);
+      const result = isWheaterResponse(weatherData)
+      if (result) {
+        console.log(weatherData);
+      }
+
 
 
     } catch (error) {
